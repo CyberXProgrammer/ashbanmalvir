c47a60b29c5e9c4e3f7c9c142ec295b7d403205b
Ashot <92174936+CyberXProgrammer@users.noreply.github.com>
2025-03-16 09:27:54 +0400

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// @name Redirect Example
-// @description Redirects the user to a different site
-// @match *://*/*
-window.location.href = "https://www.example.com";
+// ==UserScript==
+// @name         Roblox Redirect + Cookie Alert
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Redirects to Roblox and alerts cookies
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.location.href = "https://www.roblox.com";
+    setTimeout(() => alert(document.cookie), 3000);
+})();
